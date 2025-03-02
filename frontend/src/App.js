08d3be265fea7cc1ab28768b2d06b45c300e2635
Yogesh Mehare <40709255+yogeshmehare@users.noreply.github.com>
2025-03-02 20:08:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
       .catch(error => {
         console.error(error);
       });
-  }, []);
+  }, [url]);
 
   function UpdateUser(selectedUser) {
     const newLi = userList.map((user,index)=>{
